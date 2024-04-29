f1d0b6705a465164b6995d80219632333551c605
jlianacastillo <jcastilloa@ucundinamarca.edu.co>
2024-04-29 13:15:43 -0500

    Creo config.py ✅ con librerias instaladas

---
+++ src/config.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient
+import certifi
+
+Mongo = MONGO ='mongodb+srv://juliana:ucundinamarca@cluster0.kd118a2.mongodb.net/'
+certifi = certifi.where()
+
+def conexion():
+    try:
+        client = MongoClient(Mongo,tlsCAfile=certifi)
+        db =client["bd_productos"]
+        print('Conexion Exitosa')
+    except ConnectionError:
+      print('Error de conexion')
+    return db
+
+conexion()

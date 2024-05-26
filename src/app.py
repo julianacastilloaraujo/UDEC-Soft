753418092df8f171c10605387b02f9af8b07561f
jlianacastillo <jcastilloa@ucundinamarca.edu.co>
2024-05-26 16:42:38 -0500

    Nuevos cambios ✅

--- src/app.py
+++ src/app.py
@@ -1,27 +1,27 @@
 from flask import Flask, render_template, request, redirect, url_for
 from config import *
-from miembroequipo import MiembroEquipo
-
-con_bd = conexion()
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    coleccion=con_bd["MiembroEquipo"]
-    MiembrosRegistrados = coleccion.find()
-    return render_template('index.html', miembroequipo = MiembrosRegistrados)
+    return render_template('index.html')
+
+@app.route("/iniciarSesion", methods=['GET','POST'])
+def iniciarSesion():
+    return render_template('iniciarSesion.html')
+
+@app.route("/registrarse")
+def registrarse():
+    return render_template('registrarse.html')
 
 @app.route('/guardar_miembroequipo', methods=['POST'])
 def agregarMiembro():
-    coleccion =con_bd["MiembroEquipo"]
     nombre =request.form['nombre']
     cargo =request.form['cargo']
     habilidades =request.form['habilidades']
 
     if nombre and cargo and habilidades:
-        objmiembroequipo = MiembroEquipo(nombre,cargo,habilidades)
-        coleccion.insert_one(objmiembroequipo.formato_doc())
         return redirect(url_for('index'))
                         
 if __name__ == "__main__":
--- src/config.py
+++ src/config.py
@@ -1,16 +1,143 @@
-from pymongo import MongoClient
-import certifi
+import firebase_admin
+from firebase_admin import credentials, firestore, auth,storage
+import requests
 
-Mongo = MONGO ='mongodb+srv://juliana:ucundinamarca@cluster0.kd118a2.mongodb.net/'
-certifi = certifi.where()
+# Inicializa Firestore DB
+firebase_sdk = credentials.Certificate('src/udecsoft-8be27-firebase-adminsdk-wpz3v-9650bee75f')
+firebase_admin.initialize_app(firebase_sdk, {
+    'storageBucket': 'gs://udecsoft-8be27.appspot.com'
+})
+db = firestore.client()
 
-def conexion():
+def registrar_usuario(email, contraseña, rol):
     try:
-        client = MongoClient(Mongo,tlsCAfile=certifi)
-        db =client["bd_miembroequipo"]
-        print('Conexion Exitosa')
-    except ConnectionError:
-      print('Error de conexion')
-    return db
+        usuario_existente = auth.get_user_by_email(email)
+        return 'El usuario ya está registrado'
+    except firebase_admin.auth.UserNotFoundError:
+        try:
+            usuario = auth.create_user(
+                email=email,
+                password=contraseña
+            )
+            datos_usuario = {
+                'Correo': email,
+                'rol': rol
+            }
+            db.collection('usuario').document(usuario.uid).set(datos_usuario)
+            return 'Usuario registrado exitosamente'
+        except Exception as e:
+            return f'Ocurrió un error: {str(e)}'
+    except Exception as e:
+        return f'Ocurrió un error al verificar el usuario: {str(e)}'
 
-conexion()
+def iniciar_sesion(email, contraseña):
+    try:
+        firebase_auth_url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword'
+        api_key = 'AIzaSyC-Hr5qAiA6rjK2f6Sr2PCmdkGwYhqqwus'
+        payload = {
+            'email': email,
+            'password': contraseña,
+            'returnSecureToken': True
+        }
+        response = requests.post(firebase_auth_url, params={'key': api_key}, json=payload)
+        response_data = response.json()
+
+        if response.status_code == 200:
+            id_token = response_data['idToken']
+            print('Inicio de sesión exitoso')
+            return id_token
+        else:
+            error_message = response_data['error']['message']
+            print('Error en el inicio de sesión:', error_message)
+            return None
+    except Exception as e:
+        print('Ocurrió un error:', str(e))
+        return None
+
+def obtener_usuarios():
+    try:
+        users_ref = db.collection('usuario')
+        users = users_ref.stream()
+        user_list = []
+        for user in users:
+            user_data = user.to_dict()
+            user_data['id'] = user.id  
+            user_data['correo'] = user_data.pop('Correo')  
+            user_data['rol'] = user_data.pop('rol')  
+            user_list.append(user_data)
+        return user_list
+    except Exception as e:
+        return None
+
+def obtener_actividades():
+    try:
+        actividades_ref = db.collection('Actividades')
+        actividades = actividades_ref.stream()
+        lista_actividades = []
+        for actividad in actividades:
+            actividad_data = actividad.to_dict()
+            actividad_data['id'] = actividad.id 
+            actividad_data['descripcion'] = actividad_data.pop('descripcion')  
+            actividad_data['imagen_url'] = actividad_data.pop('imagen_url')  
+            actividad_data['titulo'] = actividad_data.pop('titulo')  
+            lista_actividades.append(actividad_data)
+        return lista_actividades
+    except Exception as e:
+        print(f"Error al obtener las actividades: {e}")
+        return None
+    
+def actualizar_rol_usuario(user_id, nuevo_rol):
+    try:
+        user_ref = db.collection('usuario').document(user_id)
+        user_ref.update({'rol': nuevo_rol})
+        return True
+    except Exception as e:
+        return False
+    
+
+def upload_image_to_storage(imagen):
+    try:
+        # Sube la imagen a Firebase Storage
+        blob = storage.bucket().blob(imagen.filename)
+        blob.upload_from_file(imagen)
+        
+        # Establece los permisos de acceso público para el objeto
+        blob.make_public()
+        
+        # Devuelve la URL de descarga de la imagen
+        return blob.public_url
+    except Exception as e:
+        # Maneja cualquier error que pueda ocurrir durante la carga de la imagen
+        print(f'Ocurrió un error al cargar la imagen: {str(e)}')
+        return None
+
+def agregar_actividad(data):
+    doc_ref = db.collection('Actividades').add(data)
+    return doc_ref[1].id
+
+def obtener_dato(id):
+    doc_ref = db.collection('datos').document(id)
+    doc = doc_ref.get()
+    if doc.exists:
+        return doc.to_dict()
+    else:
+        return None
+
+def actualizar_actividad(id, data):
+    try:
+        doc_ref = db.collection('Actividades').document(id)
+        doc_ref.update(data)
+        return True
+    except Exception as e:
+        print(f"Error al actualizar la actividad: {e}")
+        return False
+
+def eliminar_actividad(actividad_id):
+    try:
+        print("Eliminando actividad con ID:", actividad_id)
+        # Eliminar la actividad de la base de datos
+        db.collection('Actividades').document(actividad_id).delete()
+        return True
+    except Exception as e:
+        print(f"Error al eliminar la actividad: {e}")
+        return False
